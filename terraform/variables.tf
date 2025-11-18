6b8176795fd6dfa9025a98ea0172c76b0376be21
nicopazaa <58083127+nicopazaa@users.noreply.github.com>
2025-11-18 15:26:20 +0100

    Add AWS region and SAM artifacts bucket variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,11 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "sam_artifacts_bucket_name" {
+  description = "Globally unique S3 bucket name for SAM artifacts"
+  type        = string
+}
 
